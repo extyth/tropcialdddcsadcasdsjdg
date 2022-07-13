588deea716f052aebf6f131c5b18d38c54bf6257
extyth <109036986+extyth@users.noreply.github.com>
2022-07-14 02:30:25 +0300

    Add files via upload

---
+++ js/js-jquery.cookie.js
@@ -0,0 +1,164 @@
+
+<!DOCTYPE html>
+<html lang='ro' dir='ltr' class="dark">
+<head>
+
+<link rel="preload" href="/custom/templates/Cobalt/js/core/mod.min.js" as="script">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link rel="preload" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous" as="style">
+
+<script src="/custom/templates/Cobalt/js/core/mod.min.js"></script>
+<title>404 &bull; Story4Craft.Ro</title>
+<meta name="title" content="404 &bull; Story4Craft.Ro">
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="theme-color" content="#1d1d1d">
+<meta name="keywords" content=" " />
+<meta name="description" content=" " />
+
+<meta property="og:type" content="website" />
+<meta property="og:url" content="https://www.story4craft.ro/core/assets/js/jquery.cookie.js" />
+<meta property="og:site_name" content="Story4Craft.Ro" />
+<meta property="og:title" content="404 &bull; Story4Craft.Ro" />
+<meta property="og:description" content=" " />
+
+<meta property="twitter:url" content="https://www.story4craft.ro/core/assets/js/jquery.cookie.js">
+<meta property="twitter:title" content="404 &bull; Story4Craft.Ro">
+<meta property="twitter:description" content=" " />
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+<link rel="stylesheet" href="/custom/templates/Cobalt/css/cobalt.css?v=1.5.1">
+<link rel="preload" href="https://use.fontawesome.com/releases/v6.0.0/css/all.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+<link rel="preload" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'">
+<style>
+		
+			:root {
+		        --p-color: #1684A2;
+		    }
+		
+		    /* Primary Color */
+		    .discord-text .st0, .status-text path, .discord-icon .st0, .status-icon path {fill: var(--p-color) !important;}
+			.swal2-icon.swal2-info, .cobalt-update-box .update-icon, .header-theme, .news-title, .top-text, .error-title, .lr-title, .p-icon, .theme-text, .other-btn:after, .other-btn:focus:after, .footer-text-header:after, .widget-header-txt:after, .discord-text .icon:after, .status-text .icon:after, .wiki-page-card span strong, .dropdown-item.d-header, .rules-menu .active span, .rules-menu .active i, .welcome-title {color: var(--p-color) !important;}
+			a, a:hover, a:focus {color: var(--p-color)}
+			.color-overlay {background: linear-gradient(var(--p-color), #101010);}
+			.swal2-icon.swal2-info, .page-item.active .page-link, .user-nav-link.active, .profile-about-box, .rules-menu-link.active {border-color: #1684A2 !important;}
+			.swal2-styled.swal2-confirm, .widget-header-txt:after, .discord-text .icon:after, .status-text .icon:after, .nav-link.store-link, .footer-text-header:after, .badge-theme, #toast-container, .progress-bar, .login-overlay, .register-overlay, .vote-tabs .btn.btn-theme.active, .p-color-overlay, .modal-header, .btn-theme, .button-checkbox>.active, .spoiler .spoiler-toggle, .spoiler .spoiler-title, .page-item.active .page-link, .btn.profile-btn.active:after, .cc-window.cc-floating .cc-highlight .cc-btn.cc-allow {background-color: var(--p-color) !important;}
+			.swal2-styled.swal2-confirm:hover, .spoiler .spoiler-toggle:hover, .spoiler .spoiler-title:hover, .btn-theme:hover {background-color: var(--p-color) !important; filter: brightness(0.85)}
+			
+			/* Header */
+			.header:after {height: 227.5px; background: linear-gradient(rgba(0, 0, 0, 0.4), transparent);} .header:before {background: linear-gradient(rgba(0, 0, 0, 0.4), #101010);}
+			.header {background-position: center top !important}
+			@media (min-width: 769px) {.header {height: 475px}}
+			@media (max-width: 768px) {.header {height: 340px}}
+		    @media (min-width: 769px) {.logo {height: 300px; margin-top: 77.5px}}
+		    @media (max-width: 768px) {.logo {height: 130px; margin-top: 90px}}
+		    .status-text, .discord-text {margin-top: 203.5px}
+		    @media (max-width: 1050px) {.status-box-container .text, .discord-box-container .text {display: none}}
+		    .line-scale > div {background-color: #FFFFFF;}
+		    
+		    /* Other */
+		    .login-overlay, .register-overlay {opacity: 0.5 !important}
+		
+		</style>
+<style>.navbar-nav .nav-item:first-child {margin-left: -15px !important}</style>
+<style>.navbar-bottom .store-link:after, .navbar-bottom .store-link:focus:after, .navbar-top-new .store-link:after, .navbar-top-new .store-link:focus:after {box-shadow: 0px 0px 12px 0px #1684A2}</style>
+<style>.p-image-frame {z-index: 1; opacity: 0.7}</style>
+<style>
+				.store-box {
+					border: 3px solid #1684A2;
+					box-shadow: 0 0 10px rgb(22 132 162 / 50%);
+				}
+				.webp .store-box {
+					background: linear-gradient(to bottom, rgba(22 132 162 / 70%), rgba(22 132 162 / 70%));
+				}
+				.no-webp .store-box {
+					background: linear-gradient(to bottom, rgba(22 132 162 / 70%), rgba(22 132 162 / 70%));
+				}
+		</style>
+<link rel="stylesheet" href="/modules/Cookie Consent/assets/css/cookieconsent.min.css">
+<meta name="robots" content="noindex">
+</head>
+<body>
+<div class="container">
+<div class="row">
+<div class="col-md-6 offset-md-3">
+<div class="card error-card text-center">
+<div class="card-body">
+<span class="error-title">404</span>
+<br />
+<span> Îmi pare rău! Nu am putut găsi pagina cerută.</span>
+<br /><br />
+<div class="row">
+<div class="col-6">
+<button class="btn btn-theme float-right" onclick="javascript:history.go(-1)">Mergi înapoi</button>
+</div>
+<div class="col-6">
+<a href="/" class="btn btn-theme float-left"> Acasă</a>
+</div>
+</div>
+<br />Dacă considerați că aceasta ar putea să fie o eroare , va rugan să contactați un administrator.
+</div>
+</div>
+</div>
+</div>
+</div>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/sweetalert2@11.2.0/dist/sweetalert2.all.min.js" integrity="sha256-tGZXBB9uK3MK1zLsI8b/1gZYQcBl8TMTi52kA2RmEKc=" crossorigin="anonymous"></script>
+<script type="text/javascript" src="/core/assets/js/jquery.cookie.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/tsparticles@1.39.3/tsparticles.min.js" integrity="sha256-CGTXk49NPei0eRo1PL2NVebBLfmfE7PQlx0ktUIpM+o=" crossorigin="anonymous"></script>
+<script type="text/javascript">
+			var particles = "yes";
+			var swal_server_copy = "Now go join the server!";
+			var discord_server = "682657813406941204";
+			var mc_server = "Mc.Story4Craft.Ro";
+			var ds_box = "yes";
+			var ds_breakpoint = "1050";
+			var users_online = "users online";
+			var user_online = "user online";
+			var players_online = "players online";
+			var player_online = "player online";
+			var server_offline = "Server is offline";
+		</script>
+<script type="text/javascript" src="/modules/Cookie Consent/assets/js/cookieconsent.min.js"></script>
+<script type="text/javascript">if (window.cookieconsent === undefined) {
+  console.warn('Failed to initialise cookie consent, it may be blocked by your browser or a browser extension');
+} else {
+  window.cookieconsent.initialise({
+    onStatusChange: function() {
+      window.location.reload();
+    },
+    
+    "position": "bottom-right",
+    "type": "opt-in",
+    "palette": {
+        "button": {
+            "background": "transparent",
+            "text": "#f1d600",
+            "border": "#f1d600"
+        },
+        "popup": {
+            "background": "#000",
+            "text": "#000"
+        }
+    },
+    "content": {
+        "policy": "Cookies",
+        "message": "This website uses cookies to enhance your browsing experience.",
+        "deny": "Disallow cookies",
+        "allow": "Allow cookies",
+        "link": "More info",
+        "href": "\/cookies\/"
+    }
+
+  });
+}
+</script>
+<script type="text/javascript">var siteName = "Story4Craft.Ro", siteURL = "/", fullSiteUrl = "https://www.story4craft.ro/", page = "404", pjsPath = "/custom/templates/Cobalt/js/particles-settings.json", copied = "Copiat!", close = "Închide", loading = "Se încarcă...", cookieNotice = "Acest site utilizează cookie-uri pentru a îmbunătăți experiența dvs. de navigare. Prin înregistrarea pe site-ul nostru sau închiderea acestei notificări sunteți de acord cu utilizarea lor.", cookieAgree = "general/cookie_agree", noMessages = "Nu există mesaje noi.", newMessage1 = "Aveți un mesaj nou.", newMessagesX = "Aveți {{count}} mesaje noi.", noAlerts = "Nu aveți notificări necitite.", newAlert1 = "Aveți o notificare nouă.", newAlertsX = "Aveți {{count}} notificări noi.", debugging = "0", loggedIn = "0", cookie = "0", confirmDelete = "Sigur doriți să ștergeți acest lucru?", offline = "Offline", noPlayersOnline = "Momentan nu sunt jucători online.", bungeeInstance = "Acest server este o instanță Bungee.", online = "Online", avatarSource = "https://cravatar.eu/helmavatar/{identifier}/{size}.png", andMoreX = "şi alţii {{count}}", loadingTime = "", route = "/core/assets/js/jquery.cookie.js"</script>
+<script type="text/javascript" src="/custom/templates/Cobalt/js/core/core.js?v=9"></script>
+<script type="text/javascript" src="/custom/templates/Cobalt/js/core/user.js?v=2"></script>
+<script type="text/javascript" src="/custom/templates/Cobalt/js/core/pages.js?v=3"></script>
+<script type="text/javascript">$.getJSON('/queries/servers/', function(data) {});</script>
+<script src="//instant.page/5.1.0" type="module" integrity="sha384-by67kQnR+pyfy8yWP4kPO12fHKRLHZPfEsiSXR8u2IKcTdxD805MGUXBzVPnkLHw"></script> <script defer src="https://static.cloudflareinsights.com/beacon.min.js/v652eace1692a40cfa3763df669d7439c1639079717194" integrity="sha512-Gi7xpJR8tSkrpF7aordPZQlW2DLtzUlZcumS8dMQjwDHEnw9I7ZLyiOj/6tZStRBGtGgN6ceN6cMH8z7etPGlw==" data-cf-beacon='{"rayId":"72a575a3ec9b39a2","version":"2022.6.0","r":1,"token":"09a3c9bd4df247b69ee0ca712ab50509","si":100}' crossorigin="anonymous"></script>
+</body>
+</html>
